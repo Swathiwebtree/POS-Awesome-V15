fdfd2d5dcb8af85d2a675e0a57636a6af6adb87a
Defendicon <robin001100110@gmail.com>
2025-06-02 02:24:00 +0500

    Revert "Camera Scanning Component Add. (#66)"
    This reverts commit 904601aead5e39f83e500e9521486078027b2e4f.

--- posawesome/uninstall.py
+++ posawesome/uninstall.py
@@ -3,7 +3,6 @@ import frappe
 
 def after_uninstall():
     clear_custom_fields_and_properties()
-    remove_delivery_charges_field()
 
 
 def clear_custom_fields_and_properties():
@@ -23,11 +22,3 @@ def clear_custom_fields_and_properties():
                     print("Deleted Property Setter: ", filter)
 
     frappe.db.commit()
-
-
-def remove_delivery_charges_field():
-    # Remove posa_delivery_charges field from Sales Invoice
-    frappe.db.delete("Custom Field", "Sales Invoice-posa_delivery_charges")
-    frappe.db.delete("Custom Field", "Sales Invoice-posa_delivery_charges_rate")
-    frappe.db.commit()
-    print("Removed delivery charges fields from Sales Invoice")
