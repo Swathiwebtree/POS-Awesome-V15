39075655aa04a0ac8160a681c708d31e74764aa7
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-04 03:06:14 +0500

    Revert "fix: use selected currency in item detail update (#740)" (#741)
    This reverts commit e2c9b64de395aa9dcbd5f9e726c22f2f63f5a92e.

--- posawesome/public/js/posapp/components/pos/invoiceItemMethods.js
+++ posawesome/public/js/posapp/components/pos/invoiceItemMethods.js
@@ -1381,9 +1381,8 @@ export default {
             doctype: "Sales Invoice",
             name: "New Sales Invoice 1",
             company: this.pos_profile.company,
-            // Use current exchange rate and currency when fetching item details
-            conversion_rate: this.exchange_rate || 1,
-            currency: this.selected_currency || this.pos_profile.currency,
+            conversion_rate: 1,
+            currency: this.pos_profile.currency,
             qty: item.qty,
             price_list_rate: item.base_price_list_rate || item.price_list_rate,
             child_docname: "New Sales Invoice Item 1",
