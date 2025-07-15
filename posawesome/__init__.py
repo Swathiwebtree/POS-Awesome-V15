4b2abf2a5b74bd5157facb0c363f5a0422d9147c
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-15 19:39:12 +0500

    fix build script and cleanup (#1079)

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 try:
-	import frappe
+       import frappe
 except ModuleNotFoundError:  # pragma: no cover - frappe may not be installed during setup
-	frappe = None
+       frappe = None
 
 __version__ = "15.3.36"
 
