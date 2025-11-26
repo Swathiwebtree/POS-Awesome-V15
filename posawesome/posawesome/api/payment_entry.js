94c1863b33d29061ef6ba70d70d4527f84865135
harikrishnan-webtree <242556935+harikrishnan-webtree@users.noreply.github.com>
2025-11-26 11:13:40 +0000

    chore: format code

--- posawesome/posawesome/api/payment_entry.js
+++ posawesome/posawesome/api/payment_entry.js
@@ -1,8 +1,8 @@
-frappe.ui.form.on('Payment Entry', {
-    onload(frm) {
-        if (frappe.route_options && frappe.route_options.payment_type) {
-            frm.set_value("payment_type", frappe.route_options.payment_type);
-            frappe.route_options = null;
-        }
-    }
+frappe.ui.form.on("Payment Entry", {
+	onload(frm) {
+		if (frappe.route_options && frappe.route_options.payment_type) {
+			frm.set_value("payment_type", frappe.route_options.payment_type);
+			frappe.route_options = null;
+		}
+	},
 });
