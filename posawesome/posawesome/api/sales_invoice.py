a523231461f7d6985c1abe63a107364921ddf1b5
Hari-1710 <harikrishnanmylatty1234@gmail.com>
2026-01-02 14:42:55 +0530

    Custom feilds in Doctype

---
+++ posawesome/posawesome/api/sales_invoice.py
@@ -0,0 +1,8 @@
+import frappe
+from frappe.model.naming import make_autoname
+
+def set_job_order_number(doc, method=None):
+    if not doc.custom_job_order_number:
+        doc.custom_job_order_number = make_autoname(
+            "JOB-.YYYY.-.#####"
+        )
