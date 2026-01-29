114d317c18968708e7120cf16f2f8fde8032fe82
saikumarwebtree <saikumar@webtreeonline.com>
2026-01-29 05:01:00 +0000

    Add new POS doctypes and components
    - Added multiple new doctypes for POS system:
      - bills_listing___vehicle
      - bills_listing_vehicle
      - bills_listing_work_order
      - cash_counting_2
      - cashier
      - day_end_closing_2
      - division
      - grn_item
      - item_check
      - loyalty_card
      - payment_summary
      - petty_cash_account_entry
      - pos_terminal
      - redeemed_coupon_points
      - system_settings_2
      - vehicle_model
    - Updated frontend components:
      - Customer.vue
      - Invoice.vue
      - invoiceItemMethods.js
    - Modified shifts.py for enhanced functionality
    - Added POS user workspace

---
+++ posawesome/posawesome/doctype/day_end_closing_2/day_end_closing_2.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Youssef Restom and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DayEndClosing_2(Document):
+	pass
