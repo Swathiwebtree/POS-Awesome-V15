fabf91765f09ce70c7cd9ce43e132e23a18ae07c
Swathiwebtree <swathi@webtreeonline.com>
2026-03-17 06:02:26 +0000

    Fixed the payment entry not saved

--- posawesome/posawesome/api/payment_entry.js
+++ posawesome/posawesome/api/payment_entry.js
@@ -1,8 +1,17 @@
 frappe.ui.form.on("Payment Entry", {
 	onload(frm) {
-		if (frappe.route_options && frappe.route_options.payment_type) {
-			frm.set_value("payment_type", frappe.route_options.payment_type);
-			frappe.route_options = null;
+		if (!frm.is_new()) {
+			return;
 		}
+
+		if (
+			frappe.route_options &&
+			frappe.route_options.payment_type &&
+			frm.doc.payment_type !== frappe.route_options.payment_type
+		) {
+			frm.set_value("payment_type", frappe.route_options.payment_type);
+		}
+
+		frappe.route_options = null;
 	},
 });
