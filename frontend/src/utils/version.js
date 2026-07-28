5df378eab0b43e8f508af88e4570791b7ea80146
Swathiwebtree <201698090+Swathiwebtree@users.noreply.github.com>
2026-07-28 09:31:55 +0000

    chore: format code

--- frontend/src/utils/version.js
+++ frontend/src/utils/version.js
@@ -2,12 +2,12 @@ const buildVersion =
 	typeof __POSAWESOME_BUILD_VERSION__ !== "undefined"
 		? __POSAWESOME_BUILD_VERSION__
 		: typeof __POSAWESOME_VERSION__ !== "undefined"
-		? __POSAWESOME_VERSION__
-		: typeof window !== "undefined" && window.__POSAWESOME_BUILD_VERSION__
-			? window.__POSAWESOME_BUILD_VERSION__
-			: typeof window !== "undefined" && window.__POSAWESOME_VERSION__
-				? window.__POSAWESOME_VERSION__
-			: "";
+			? __POSAWESOME_VERSION__
+			: typeof window !== "undefined" && window.__POSAWESOME_BUILD_VERSION__
+				? window.__POSAWESOME_BUILD_VERSION__
+				: typeof window !== "undefined" && window.__POSAWESOME_VERSION__
+					? window.__POSAWESOME_VERSION__
+					: "";
 
 export const POSAWESOME_VERSION = buildVersion;
 
